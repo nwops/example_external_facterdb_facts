76fd5545def4a310016069aef834e2da4fa4c838
Corey Osman <corey@logicminds.biz>
2019-02-19 23:30:24 -0800

    update script

--- get_facts.rb
+++ get_facts.rb
@@ -4,25 +4,25 @@ require 'json'
 
 # connects to puppetdb and creates file for all the systems it finds
 
-puppetdb_conf = File.join('~/.puppetlabs/client-tools/puppetdb.conf')
+puppetdb_conf = File.expand_path File.join('~/.puppetlabs/client-tools/puppetdb.conf')
 conn = JSON.parse(puppetdb_conf)['puppetdb']
 
 def client
-  @client |= begin
+  @client ||= begin
     PuppetDb::Client.new({
       server: conn['server_urls'].first,
       pem: { 'key' => conn['key'], 'cert' => conn['cert'], 'ca_file' => conn['cacert'] },
-      
+
     })
   end
 end
 
-response = client.request('', 'factsets[] { }', {:limit => 100}.data.map do |set|
+response = client.request('', 'factsets[] { }', {:limit => 100}).data.map do |set|
   facts = set['facts']["data"]
   facts_hash = {}
-  facts.each {|item| facts_hash[item['name'] = item['value'] }
+  facts.each {|item| facts_hash[item['name']] = item['value'] }
   dir = File.join('facts', facts_hash['facterversion'], facts_hash['kernel'], facts_hash.fetch('node_role', 'none'))
-  FileUtils.mdir(dir)
+  FileUtils.mkdir_p(dir)
   File.write(File.join(dir, "#{facts_hash['fqdn']}.facts"), JSON.pretty_generate(facts_hash))
   facts_hash
 end
